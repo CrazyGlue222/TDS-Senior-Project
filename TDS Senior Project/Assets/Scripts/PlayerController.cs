2a1728159a07537ac51aa5c49c6c4fd7f013b08b
CrazyGlue222 <cobra100705@gmail.com>
2019-10-04 19:37:26 -0700

    Platformer

---
+++ TDS Senior Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        if (Input.GetButtonDown("Jump") && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        } else if(Input.GetButtonDown("Jump") && extraJumps == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+}
